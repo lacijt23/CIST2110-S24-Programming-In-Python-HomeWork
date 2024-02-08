7842408e0997dbb7759cd4b2501c38c7c96274a6
Laci Trull <trulll@go.stockton.edu>
2024-02-08 12:30:54 -0500

    homework 1

--- HW1.py
+++ HW1.py
@@ -1,69 +1,75 @@
 # HW1.py
-# Author:
+# Author: Laci Trull
 
 # Question 1:
 # Print Hello World like we did in class
-
+print("Hello World")
 # Question 2:
 # Print the following:
 # Your name
-
+print("Laci")
 # Your age
-
+print(20)
 # Your favorite color
-
+print("Purple")
 # Your favorite animal
-
+print("Cat")
 # Question 3:
 # Create a variable called "myName" and set it equal to your name
-
+myName = "Laci"
 # Create a variable called "myAge" and set it equal to your age
-
+myAge = 20
 # Create a variable called "myColor" and set it equal to your favorite color
-
+myColor = "Purple"
 # Create a variable called "myAnimal" and set it equal to your favorite animal
-
+myAnimal = "Cat"
 # Print the following:
 # Hello, my name is <myName>
+print("Hello, my name is " + myName + "")
 # I am <myAge> years old
+print("I am " + str(myAge) + " years old")
 # My favorite color is <myColor>
+print("My favorite color is " + myColor + "")
 # My favorite animal is <myAnimal>
-
+print("My favorite animal is " + myAnimal + "")
 
 # Question 4:
 # Calculate the following and print the result:
 # 2 + 2
-
+print(2 + 2)
 # 3 * 4
-
+print(3 * 4)
 # 5 - 6
-
+print(5 - 6)
 # 8 / 2
-
+print(8/2)
 # Question 5:
 # Create a variable called "num1" and set it equal to 2
-
+num1 = 2
 # Create a variable called "num2" and set it equal to 3
-
+num2 = 3
 # Create a variable called "num3" and set it equal to 4
-
+num3 = 4
 # Create a variable called "num4" and set it equal to 5
-
+num4 = 5
 # Calculate the following and print the result:
 # num1 + num2
-
+print(num1 + num2)
 # num3 * num4
-
+print(num3 + num4)
 # num4 - num1
-
+print(num4 - num1)
 # num2 / num1
-
+print(num2 / num1)
 # Question 6: Write a program that asks the user for their name and then prints the following:
-
+print("What is your name?")
+name = input()
 # Hello, <name>. Please enter three numbers.
-
+print("Hello " + name + ". Please enter three numbers.")
 # The program should then ask the user for three numbers (floats) and print the following:
-
+inputNum1 = float(input())
+inputNum2 = float(input())
+inputNum3 = float(input())
 
 # 1. The sum of the three numbers is <sum>
 
--- README.md
+++
@@ -1,3 +0,0 @@
-### CIST2110-S24-Programming-In-Python-HomeWork
-
-This is a repository for my homework assignments for CIST 2110 Programming in Python
